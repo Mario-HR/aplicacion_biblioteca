cac9f84570a02c4f39d701028551e28bc51246a5
Mario Rodarte <mariohrodarte@outlook.com>
2024-10-31 23:52:35 -0600

    Vista prncipal

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.plantilla')
+
+@section('titulo','Vista principal')
+
+@section('contenido')
+<img src="{{asset("images/biblioteca.jpg")}}" alt="">
+<h1>24 de octubre. Día de las Bibliotecas</h1>
+El Día de las Bibliotecas se celebra cada 24 de octubre desde el año 1997. Este día nació como iniciativa de la Asociación Española de Amigos del Libro Infantil y Juvenil con el objetivo de concienciar a la sociedad de la importancia de la lectura y como homenaje y reconocimiento a la labor de los bibliotecarios/as.
+
+«El lema de este año, «Por un futuro sostenible», pretende destacar el papel fundamental que desempeñan todas las tipologías de bibliotecas (públicas, escolares, universitarias, especializadas y nacionales/regionales) en la consecución de los Objetivos de Desarrollo Sostenible de la Agenda 2030.
+
+A través de sus colecciones, servicios y actividades con usuarios, las bibliotecas contribuyen a construir una sociedad cada vez mejor informada, más igualitaria, solidaria y justa, plenamente consciente de la necesidad de llevar un modo de vida sostenible como compromiso con las generaciones futuras.»
+@endsection
